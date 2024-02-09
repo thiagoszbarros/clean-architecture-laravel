41031ebddbaabf28da087ed686d5db41f25e8dcd
Thiago Barros <thiagobarros95@gmail.com>
2024-02-09 14:13:49 -0300

    refactor: alter v1 routes dir

--- app/Presenter/Http/Routes/api.php
+++ app/Presenter/Http/Routes/api.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-foreach (File::allFiles(__DIR__ . '/v1') as $partial) {
+foreach (File::allFiles(__DIR__ . '/api/v1') as $partial) {
     Route::prefix('/v1')->group($partial->getPathname());
 }
